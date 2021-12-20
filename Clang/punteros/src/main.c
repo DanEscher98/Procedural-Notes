fcc138796683b84eecf21984288e98f7f069ab0a
Danyiel Colin <danyiel5978@gmail.com>
2021-12-20 02:12:43 -0600

    modified:   Makefile modified:   hello.nasm modified:   ../Clang/punteros/src/main.c

--- Clang/punteros/src/main.c
+++ Clang/punteros/src/main.c
@@ -1,8 +1,14 @@
 #include "lib.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-	printf("Hello %s!\n", argv[1]);
+int main(int argc, char** argv)
+{
+	char saludo[5] = { 104, 111, 108, 32, 97 };
+	for (int i = 0; i < 5; i++) {
+		printf("%c", saludo[i]);
+	}
+	// int a = 5;
 	return EXIT_SUCCESS;
 }
