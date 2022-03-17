09bc8f855ad47eba53d7a1599d585c3ddaaba25a
Danyiel Colin <danyiel5978@gmail.com>
2022-03-16 21:23:25 -0500

    modified:   Makefile modified:   README.md modified:   src/main.c

--- Clang/IOactions/options/README.md
+++ Clang/IOactions/options/README.md
@@ -1,1 +1,3 @@
 # options
+Hello, this is a simple template to parse arguments in C.
+It could take a file and prints its contents.
--- Clang/IOactions/options/src/main.c
+++ Clang/IOactions/options/src/main.c
@@ -1,8 +1,50 @@
 #include "lib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+
+static float version = 1.0;
+void print_help(void);
+void printfile(char*);
+
+void print_help(void) {
+	printfile("help.txt");
+}
+
+void printfile(char *filename) {
+	FILE *fptr;
+	if ((fptr = fopen(filename, "r")) == NULL) {
+		printf("Cannot open file.\n");
+		exit(EXIT_FAILURE);
+	} else {
+		for (char c = fgetc(fptr); c != EOF; c = fgetc(fptr)) {
+			putc(c, stdout);
+		}
+	}
+}
 
 int main(int argc, char **argv) {
-	printf("Hello %s!\n", argv[1]);
+	int opt, option_index;
+	static struct option long_opts[] = {
+		{ "file", true, 0, 'f'},
+		{ "version", false, 0, 'v' },
+		{ "help", false, 0, 'h' },
+		{ NULL, 0, 0, 0 }
+	};
+	while ((opt = getopt_long(argc, argv, "f:vh", long_opts, &option_index)) != -1) {
+		switch (opt) {
+			case 'f':
+				printfile(optarg);
+				break;
+			case 'v':
+				printf("v%.2f\n", version);
+				exit(EXIT_SUCCESS);
+				break;
+			case 'h':
+				print_help();
+				break;
+		}
+	}
 	return EXIT_SUCCESS;
 }
