197a3f39fbc4fbc0ba4b16fef94c114f619461be
Danyiel Colin <danyiel5978@gmail.com>
2023-06-03 22:27:34 -0600

    Args as class: 2023:06:03

--- Cpp-20/basic_layout/src/cli.hpp
+++ Cpp-20/basic_layout/src/cli.hpp
@@ -4,57 +4,43 @@
 
 class Args {
 public:
-  const std::string name;
-  const int threads;
-  const std::optional<int> optionalField;
+  std::string name;
+  int threads;
+  std::optional<int> optionalField;
 
   // Delete the copy constructor and assignment operator to prevent copying
   Args(const Args&) = delete;
   Args& operator=(const Args&) = delete;
 
-  // This static method ensures that Args instance is initialized only once and is thread-safe
-  static const Args& getInstance(int argc = 0, char* argv[] = nullptr) {
+  static Args& getInstance(int argc = 0, char* argv[] = nullptr) {
     static Args instance(argc, argv);
     return instance;
   }
 
 private:
   // Make the constructor private so that only getInstance can access it
-  Args(int argc, char* argv[]) :
-    name(parseOption<std::string>("n,name", "Name to greet", "World", argc, argv)),
-    threads(parseOption<int>("t,threads", "Number of threads", 4, argc, argv)),
-    optionalField(parseOptionalOption("o,optional", "Optional integer field", argc, argv))
-  {}
+  Args(int argc, char* argv[]) {
+    cxxopts::Options options("helloworld", "A hello world program");
 
-  // Helper function to setup and parse options
-  template<typename T>
-  auto setupAndParseOptions(const std::string& option, const std::string& description, int argc, char* argv[], const T& defaultValue = T()) {
-    cxxopts::Options options("helloworld", fmt::format("A hello world program with options: {}", description));
-    options.add_options()(option, description, cxxopts::value<T>()->default_value(defaultValue));
-    options.add_options()("h,help", "Print usage");
+    options.add_options()
+      ("n,name", "Name to greet", cxxopts::value<std::string>()->default_value("World"))
+      ("t,threads", "Number of threads", cxxopts::value<int>()->default_value("4"))
+      ("o,optional", "Optional integer field", cxxopts::value<int>())
+      ("h,help", "Print usage")
+    ;
+
     auto result = options.parse(argc, argv);
-
     if (result.count("help")) {
       fmt::print("{}", options.help());
       exit(0);
     }
 
-    return result[option];
-  }
+    name = result["name"].as<std::string>();
+    threads = result["threads"].as<int>();
 
-  // Template function to parse options
-  template<typename T>
-  T parseOption(const std::string& option, const std::string& description, const T& defaultValue, int argc, char* argv[]) {
-    return setupAndParseOptions<T>(option, description, argc, argv, defaultValue).as<T>();
-  }
-
-  // Function to parse optional integer option
-  std::optional<int> parseOptionalOption(const std::string& option, const std::string& description, int argc, char* argv[]) {
-    auto result = setupAndParseOptions<int>(option, description, argc, argv);
-    if (result.count()) {
-      return result.as<int>();
-    } else {
-      return std::nullopt;
+    // If the optional field was given, store its value
+    if (result.count("optional")) {
+      optionalField = result["optional"].as<int>();
     }
   }
 };
--- Cpp-20/basic_layout/src/main.cpp
+++ Cpp-20/basic_layout/src/main.cpp
@@ -3,7 +3,7 @@
 #include "utils.hpp"
 
 int main(int argc, char* argv[]) {
-  Args args = parseArguments(argc, argv);
+  Args& args = Args::getInstance(argc, argv);
   fmt::print("Hi there {}!", fmt::format(fg(fmt::color::green), args.name));
   write_file(args.threads);
   return 0;
